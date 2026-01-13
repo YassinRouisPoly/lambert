ccde3aa8dd53bebf8cc5aa571cc4789c5a723713
Yassin ROUIS <48293288+yassin-rouis@users.noreply.github.com>
2026-01-13 13:30:47 +0100

    T.U.D.

--- optimized_train.py
+++ optimized_train.py
@@ -1,13 +1,14 @@
-import model_predict
+import server.model_predict
 import optuna
-import pipeline
+import pipeline as pipel
+from zenml import pipeline
 
 def objective(trial):
-    dataset_train, dataset_test = pipeline.load_datasets(version="latest")
+    dataset_train, dataset_test = pipel.load_datasets(version="latest")
     epochs = trial.suggest_int("epochs", 1, 10)
     per_device_batch_size = trial.suggest_int("per_device_batch_size", 1, 8)
     learning_rate = trial.suggest_float("learning_rate", 1e-5, 1e-4)
-    model_cache = pipeline.train_model(
+    model_cache = pipel.train_model(
         train_dataset=dataset_train,
         eval_dataset=dataset_test,
         output_dir="./cache/output",
@@ -17,7 +18,7 @@ def objective(trial):
         per_device_eval_batch_size=8,
         learning_rate=learning_rate,
     )
-    evals = pipeline._eval_model(pipeline._get_model(model_cache))
+    evals = pipel._eval_model(pipel._get_model(model_cache))
     return evals["loss"]
 
 @pipeline
